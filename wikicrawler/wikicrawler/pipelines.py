4ef0ae5da5e9217bb2678441aeb9e03f79db4ae5
hanaaaaa <34371356+hanaaaaa@users.noreply.github.com>
2017-12-10 15:14:16 +0800

    Update pipelines.py
    Count...although I don't know when it can be finished.

--- wikicrawler/wikicrawler/pipelines.py
+++ wikicrawler/wikicrawler/pipelines.py
@@ -5,13 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
-
+i = 0
 class FilePipeline(object):
     def __init__(self):
-        self.filename = open("entries.json","wb")
+        self.filename = open("entries.json","w")
     def process_item(self, item, spider):
+        global i
+        i = i + 1
         jsontext = json.dumps(dict(item), ensure_ascii=False) + ",\n"
-        self.filename.write(jsontext.encode('utf-8'))
+        self.filename.write(jsontext)
         return item
     def close_spider(self, spider):
+        global i
+        self.filename.write("\n共" + str(i) + "字条")
         self.filename.close()
